c7593ddc7f54010200e9adc5e2e080c6d71476ab
ishika30k <ishikakh30@gmail.com>
2025-10-10 18:57:03 +0530

    added test cases

--- tests/testcase.py
+++ tests/testcase.py
@@ -1,28 +1,67 @@
 import pytest 
+from app import app
 
-def valid_login(client):
+def test_homepage(client):
+    response = client.get('/')
+    assert response.status_code == 200
+
+
+def test_valid_login(client):
     """Test login with valid cred"""
-    response=client.post("/login", data={
+    response=client.post('/login', data={
         "username":"ishika12",
         "password":"Ishika12"
-    })
+    }, follow_redirects=True)
     assert response.status_code == 200
-    assert b"dashboard" in response.data
+    assert b"Skin Info" in response.data
 
-def login_invalid_password(client):
+def test_login_invalid_password(client):
     """Login with Invalid Password"""
     response=client.post("/login", data={
         "username":"ishika12",
         "password":"wrongpass"
-    })
+    }, follow_redirects=True)
     assert response.status_code == 200
-    assert b"Incorrect password" in response.data
+    assert b"Invalid username or password" in response.data
 
-def login_empty_fields(client):
+def test_login_empty_fields(client):
     """Test login with empty fields"""
     response=client.post("/login", data={
         "username":"",
         "password":""
-    })
+    }, follow_redirects=True)
     assert response.status_code == 200
-    assert b"Fields cannot be empty" in response.data
+    assert b"Invalid username or password" in response.data
+
+# def test_valid_signin(client):
+#     """Test signin with valid cred"""
+#     response=client.post('/signin', data={
+#         "username":"girl123",
+#         "name":"girl g",
+#         "password":"Girl1235",
+#         "phone":"8327927365",
+#         "email":"girl30@gmail.com",
+#         "gender":"female",
+#         "age":"23"
+#     }, follow_redirects=True)
+#     assert response.status_code == 200
+#     assert b"Account created successfully! Please login." in response.data
+
+def test_signin_existing_username(client):
+    """Test signin with existing username"""
+    response=client.post('/signin', data={
+        "username":"ishika12",
+        "name":"ishika g",
+        "password":"Ishika123",
+        "phone":"8327927364",
+        "email":"ishika30@gmail.com",
+        "gender":"female",
+        "age":"23"
+    }, follow_redirects=True)
+    assert response.status_code == 200
+    assert b"This username is already taken. Please choose another." in response.data
+
+def 
+
+
+
