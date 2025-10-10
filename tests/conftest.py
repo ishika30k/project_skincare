442c994e6e3685a1ccac974ed8328101b8f6efea
ishika30k <ishikakh30@gmail.com>
2025-10-10 18:55:42 +0530

    added test cases

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,22 @@ import sys, os
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from app import app as flask_app   # import the real Flask app
+from app import app as flask_app  
+
+from unittest.mock import MagicMock
+
+@pytest.fixture
+def mock_db(mocker):
+    mock_cursor = MagicMock()
+    mock_cursor.fetchone.return_value = None
+    mock_cursor.execute.return_value = True
+
+    mock_conn = MagicMock()
+    mock_conn.cursor.return_value = mock_cursor
+
+    mocker.patch("app.mysql.connection", mock_conn)
+    return mock_conn, mock_cursor
+
 
 @pytest.fixture()
 def app():
